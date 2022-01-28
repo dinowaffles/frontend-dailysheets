4189572b0052a460fe51c9c1fcef46a101c2c869
Abby Henes <abbyhenes@gmail.com>
2022-01-28 09:57:48 -0600

    fleshed out student and classroom components.ts

--- src/app/classroom-list/classroom-list.component.ts
+++ src/app/classroom-list/classroom-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Classroom } from '../classroom';
+import { ClassroomService } from '../classroom.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-classroom-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassroomListComponent implements OnInit {
 
-  constructor() { }
+  classrooms: Classroom[] | undefined;
+
+  constructor(private classroomService: ClassroomService, 
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.getClassrooms();
+  }
+
+  private getClassrooms() {
+    this.classroomService.getClassroomList().subscribe(data => {
+      this.classrooms = data;
+    });
+  }
+
+  updateClassroom(id: number) {
+    this.router.navigate(['classroom-update', id]);
+  }
+
+  deleteClassroom(id: number) {
+    this.classroomService.deleteClassroom(id).subscribe(data => {
+      console.log(data);
+      this.getClassrooms();
+    })
   }
 
 }
--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
+import { StudentService } from '../student.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-student-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentListComponent implements OnInit {
 
-  constructor() { }
+  students: Student[] | undefined;
+
+  constructor(private studentService: StudentService, 
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.getStudents();
+  }
+
+  private getStudents() {
+    this.studentService.getStudentList().subscribe(data => {
+      this.students = data;
+    });
+  }
+
+  updateStudent(id: number) {
+    this.router.navigate(['student-update', id]);
+  }
+
+  deleteStudent(id: number) {
+    this.studentService.deleteStudent(id).subscribe(data => {
+      console.log(data);
+      this.getStudents();
+    })
   }
 
 }
