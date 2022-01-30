7ff20431c012676175db9ba89c9c80d307b68a6d
Abby Henes <abbyhenes@gmail.com>
2022-01-29 22:19:55 -0600

    working on update student/classroom links

--- src/app/classroom-list/classroom-list.component.ts
+++ src/app/classroom-list/classroom-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Classroom } from '../classroom';
 import { ClassroomService } from '../classroom.service';
 import { Router } from '@angular/router';
-
+ 
 @Component({
   selector: 'app-classroom-list',
   templateUrl: './classroom-list.component.html',
--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Classroom } from '../classroom';
+import { ClassroomService } from '../classroom.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-teacher',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherComponent implements OnInit {
 
-  constructor() { }
+  classrooms: Classroom[] | undefined;
+
+  constructor(private classroomService: ClassroomService, 
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.getClassrooms();
+  }
+
+  private getClassrooms() {
+    this.classroomService.getClassroomList().subscribe(data => {
+      this.classrooms = data;
+    });
   }
 
 }
