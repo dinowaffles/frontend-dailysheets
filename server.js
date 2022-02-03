c9044713cfb69a80233de40ef8b4766e53b69bd4
Abby Henes <abbyhenes@gmail.com>
2022-02-02 21:19:04 -0600

    lets try heroku deploy again

--- server.js
+++ server.js
@@ -7,19 +7,19 @@
 // }
 
 //Install express server
-// const express = require('express');
-// const path = require('path');
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// // Serve only the static files form the dist directory
-// // app.use(requireHTTPS);
-// app.use(express.static(__dirname + '/dist/dailysheets-frontend'));
+// Serve only the static files form the dist directory
+// app.use(requireHTTPS);
+app.use(express.static(__dirname + './dist/dailysheets-frontend'));
 
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname+'/dist/dailysheets-frontend/index.html'));
-// });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'./dist/dailysheets-frontend/index.html'));
+});
+
